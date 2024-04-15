dd31c5a2ae39d89f5a4a61faba6d11e51cc57e69
Priyanka-Shakya <priyankashakya1404@gmail.com>
2024-04-15 14:21:30 +0530

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <ion-icon name="code-slash-outline"></ion-icon>
+      <h2>CodeEditor</h2>
+    </div>
+  )
+}
+
+export default Header;
